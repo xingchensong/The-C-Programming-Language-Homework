17c7076fd570a42a22f172b36b87329b5f160cd1
xingchensong <xingchensong1996@163.com>
2022-10-15 12:06:04 +0800

    feat(ch01): hello world

---
+++ 导言/hello_world.cc
@@ -0,0 +1,8 @@
+// Copyright [2022-10-15] <sxc19@mails.tsinghua.edu.cn, Xingchen Song>
+
+#include <stdio.h>
+
+int main() {
+  printf("hello world\n");
+  return 0;
+}
---
+++ 导言/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Copyright [2022-10-15] <sxc19@mails.tsinghua.edu.cn, Xingchen Song>
+
+gcc ./hello_world.cc
+./a.out
+rm -f ./a.out
